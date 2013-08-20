c17d9fdd4a1f4684495d789bb80c053c6aaed0ad
David Joos <david.joos@gmail.com>
2013-08-20 22:05:39 +0100

    fix chef syntax error, fixes #3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures Yasm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 %w{ debian ubuntu centos redhat fedora scientific amazon }.each do |os|
 supports os
