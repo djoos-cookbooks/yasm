08aca0a0ccf4a3248165275943c5d99bfaab1f2a
David Joos <david.joos@gmail.com>
2014-11-15 20:13:01 +0000

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dev@escapestudios.com'
 license 'MIT'
 description 'Installs/Configures Yasm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.1'
+version '1.0.2'
 
 %w( debian ubuntu centos redhat fedora scientific amazon ).each do |os|
   supports os
